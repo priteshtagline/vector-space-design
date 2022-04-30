2c3f925aacf7dcc29187431bed97c1dd536f5e03
VivekTagline <vivek.tagline@gmail.com>
2022-04-30 16:09:12 +0530

    Home page slider change

--- js/script.js
+++ js/script.js
@@ -1,13 +1,24 @@
-// if ($("#fullpage").length > 0) {
-//   var myFullpage = new fullpage("#fullpage", {
-//     anchors: ["content1", "content2", "content3", "content4", "content5", "content6"],
-//     menu: "#menu",
-//     navigation: false,
-//     navigationTooltips: ["First page", "Second page", "Third and last page"],
-//     responsiveWidth: 900,
-//     afterResponsive: function (isResponsive) {},
-//   });
-// }
+if ($(".main-page-slider").length > 0) {
+  $(".main-page-slider").slick({
+    dots: true,
+    infinite: false,
+    arrows: false,
+    vertical: true,
+    verticalSwiping: true,
+    speed: 500,
+    slidesToShow: 1,
+  });
+  // const slider = $(".main-page-slider");
+
+  // slider.on("wheel", function (e) {
+  //   if (e.originalEvent.deltaY < 0) {
+  //     $(this).slick("slickPrev");
+  //   } else {
+  //     e.preventDefault();
+  //     $(this).slick("slickNext");
+  //   }
+  // });
+}
 
 $(".background_bg").each(function () {
   var attr = $(this).attr("data-img-src");
