f810eb955970cc39fb18b8b7fbc89bcc15523203
VivekTagline <vivek.tagline@gmail.com>
2022-05-02 15:31:05 +0530

    Change the navbar menu design in mobile view

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Home page slider
 if ($("#fullpage").length > 0) {
   var myFullpage = new fullpage("#fullpage", {
     anchors: ["content1", "content2", "content3", "content4", "content5", "content6"],
@@ -15,9 +16,26 @@ if ($("#fullpage").length > 0) {
   });
 }
 
+// Inline image src generate
 $(".background_bg").each(function () {
   var attr = $(this).attr("data-img-src");
   if (typeof attr !== typeof undefined && attr !== false) {
     $(this).css("background", "url(" + attr + ") center center/cover");
   }
 });
+
+// Navbar toggle JQuery
+if (window.innerWidth < 992) {
+  $(".sidebar-toggle").on("click", function () {
+    if ($(".navbar-collapse").hasClass("show")) {
+      setTimeout(function () {
+        $("body").removeClass("background-blur");
+      }, 0);
+    } else {
+      setTimeout(function () {
+        $("body").addClass("background-blur");
+      }, 300);
+    }
+    $(this).toggleClass("open");
+  });
+}
